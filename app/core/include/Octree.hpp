38546c5d1f6bb63102c26a17af29d722ae4dac0b
monkeysoop <istok.endre2@gmail.com>
2025-01-05 00:02:36 +0100

    octree compression into buffer

--- app/core/include/Octree.hpp
+++ app/core/include/Octree.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <array>
 #include <memory>
+#include <cstdint>
 
 #include "Mesh.hpp"
 
@@ -21,17 +22,24 @@ struct OctreeNode {
 
 class Octree {
 public:
-    Octree(std::vector<Mesh> meshes, size_t depth_limit, size_t max_triangles_per_node);
+    Octree(std::vector<Mesh> meshes, size_t depth_limit, size_t max_triangles_per_node, size_t max_triangles_per_leaf, size_t m_keep_triangles_after_this_many_overlaps);
     ~Octree();
 
+    std::vector<uint32_t> m_compressed_node_buffer;
+    std::vector<uint32_t> m_compressed_triangle_indecies;
+
 private:
-    void Traverse(std::unique_ptr<OctreeNode>& node, size_t current_depth);
+    void DepthFirstCompress(std::unique_ptr<OctreeNode>& node, size_t parent_node_child_pointer_location);
+    void DepthFirstTraverse(std::unique_ptr<OctreeNode>& node, size_t current_depth);
     void Subdivide(std::unique_ptr<OctreeNode>& node, size_t current_depth);
 
 
     std::vector<glm::vec3> m_vertecies;
     std::unique_ptr<OctreeNode> m_root;
     size_t m_max_depth;
-    const size_t m_depth_limit;
-    const size_t m_max_triangles_per_node; // a leaf could have more triangles than this limit if it is at the depth limit
+
+    const size_t m_depth_limit; // inclusive
+    const size_t m_max_triangles_per_node; // it shouldn't have more than 65536 (2^16)
+    const size_t m_max_triangles_per_leaf; // a leaf could have more triangles than this limit if it is at the depth limit, it shouldn't have more than 65536 (2^16)
+    const size_t m_keep_triangles_after_this_many_overlaps; // if lets say this is set at 5 and a triangle intersects at least 5 childrens aabb than that triangle won't be copied into the childrens rather it will be kept in the node 
 };
--- app/core/src/Octree.cpp
+++ app/core/src/Octree.cpp
@@ -6,7 +6,9 @@
 #include <numeric>
 #include <algorithm>
 
+
 #include <iostream>
+#include <stdio.h>
 
 bool AABBTriangleOverlapTest(AABB aabb, glm::vec3 v1, glm::vec3 v2, glm::vec3 v3) {
     glm::vec3 box_center{(aabb.min_bounds + aabb.max_bounds) / 2.0f};
@@ -15,32 +17,87 @@ bool AABBTriangleOverlapTest(AABB aabb, glm::vec3 v1, glm::vec3 v2, glm::vec3 v3
     return triBoxOverlap(box_center, box_half_size, v1, v2, v3);
 }
 
-void Octree::Traverse(std::unique_ptr<OctreeNode>& node, size_t current_depth) {
+void Octree::DepthFirstCompress(std::unique_ptr<OctreeNode>& node, size_t parent_node_child_pointer_location) {
+    if (parent_node_child_pointer_location != 0) {
+        uint32_t node_start = static_cast<uint32_t>(m_compressed_node_buffer.size());
+        m_compressed_node_buffer[parent_node_child_pointer_location] = node_start;
+    }
 
+    uint16_t triangle_count = static_cast<uint16_t>(node->triangles.size());
+    uint8_t children_mask = 0x00;
+    uint8_t children_count = 0; 
+    
+    if (!node->is_leaf) {
+        for (size_t i = 0; i < 8; i++) {
+            if (!node->childrens[i]->is_leaf) {
+                children_mask |= (0x01 << i);
+                children_count++;
+            }
+        }
+    }
+    
+    uint32_t node_info = (triangle_count << 16) | (children_mask << 8) | children_count;
+    uint32_t triangle_start = static_cast<uint32_t>(m_compressed_triangle_indecies.size());
+
+    //printf("node: 0x%04x  0x%02x  0x%02x  %d\n", triangle_count, children_mask, children_count, node->is_leaf);
+
+    for (const glm::uvec3& ind : node->triangles) {
+        m_compressed_triangle_indecies.push_back(static_cast<uint32_t>(ind.x));
+        m_compressed_triangle_indecies.push_back(static_cast<uint32_t>(ind.y));
+        m_compressed_triangle_indecies.push_back(static_cast<uint32_t>(ind.z));
+    }
+    
+    m_compressed_node_buffer.push_back(node_info);
+    m_compressed_node_buffer.push_back(triangle_start);
+
+    size_t child_pointers_start = m_compressed_node_buffer.size();
+    
+    for (size_t i = 0; i < children_count; i++) {
+        uint32_t placeholder = 0; // when the child node is called it will overwrite this
+        m_compressed_node_buffer.push_back(placeholder);
+    }
+
+    size_t child_pointer_offset = 0;
+    for (size_t i = 0; i < 8; i++) {
+        if (children_mask & (0x01 << i)) {
+            DepthFirstCompress(node->childrens[i], (child_pointers_start + child_pointer_offset));
+            child_pointer_offset++;
+        }
+    }
+
+}
+
+void Octree::DepthFirstTraverse(std::unique_ptr<OctreeNode>& node, size_t current_depth) {
     for (size_t i = 0; i < current_depth; i++) {
         std::cout << '\t';
     }
     std::cout << "size: " << node->triangles.size();
 
-    std::cout << (node->is_leaf ? " leaf" : " node") << std::endl;
+    std::cout << (node->is_leaf ? " leaf" : " node") << " depth: " << current_depth << std::endl;
 
     if (!node->is_leaf) {
         for (size_t i = 0; i < 8; i++) {
-            if (node->childrens[i]->triangles.size() != 0) {
+            if (!node->childrens[i]->is_leaf || node->childrens[i]->triangles.size() != 0) {
                 //std::cout << node->childrens[i]->triangles.size() << std::endl;
-                Traverse(node->childrens[i], current_depth + 1);
+                DepthFirstTraverse(node->childrens[i], current_depth + 1);
             }
         }
     }
 }
 
 void Octree::Subdivide(std::unique_ptr<OctreeNode>& node, size_t current_depth) {
-    std::cout << "depth: " << current_depth << std::endl;
-    if (current_depth >= m_depth_limit || node->triangles.size() <= m_max_triangles_per_node) {
+    //std::cout << "depth: " << current_depth << std::endl;
+
+    if (current_depth >= m_depth_limit) {
         return;
-    } else {
-        node->is_leaf = false;
+    } else if (node->triangles.size() <= m_max_triangles_per_leaf) {
+        m_max_depth = std::max(m_max_depth, current_depth);
+        return;
     }
+    m_max_depth = std::max(m_max_depth, current_depth);
+
+    
+    node->is_leaf = false;
 
     glm::vec3 mid_point{(node->bounding_box.min_bounds + node->bounding_box.max_bounds) / 2.0f};
 
@@ -89,7 +146,7 @@ void Octree::Subdivide(std::unique_ptr<OctreeNode>& node, size_t current_depth) 
             }
         }
 
-        if (childrens_overlap.size() <= 2) {
+        if (childrens_overlap.size() < m_keep_triangles_after_this_many_overlaps) {
             for (size_t child_index : childrens_overlap) {
                 node->childrens[child_index]->triangles.push_back(ind);
             }
@@ -100,7 +157,7 @@ void Octree::Subdivide(std::unique_ptr<OctreeNode>& node, size_t current_depth) 
         //std::cout << overlapped_childrens.size() << std::endl;
     }
 
-    std::cout << "\tsize: " << childrens_overlappings.size() << std::endl;
+    //std::cout << "\tsize: " << childrens_overlappings.size() << std::endl;
 
     if (childrens_overlappings.size() > m_max_triangles_per_node) {
         //for (auto i : childrens_overlappings) {
@@ -145,7 +202,7 @@ void Octree::Subdivide(std::unique_ptr<OctreeNode>& node, size_t current_depth) 
     //    }
     //}
 
-    std::cout << "\tsize: " << kept_triangle_indecies.size() << std::endl;
+    //std::cout << "\tsize: " << kept_triangle_indecies.size() << std::endl;
 
 
     node->triangles.swap(kept_triangle_indecies);
@@ -168,7 +225,13 @@ void Octree::Subdivide(std::unique_ptr<OctreeNode>& node, size_t current_depth) 
 
 }
 
-Octree::Octree(std::vector<Mesh> meshes, size_t depth_limit, size_t max_triangles_per_node) : m_depth_limit{depth_limit}, m_max_triangles_per_node{max_triangles_per_node} {
+Octree::Octree(std::vector<Mesh> meshes, size_t depth_limit, size_t max_triangles_per_node, size_t max_triangles_per_leaf, size_t m_keep_triangles_after_this_many_overlaps) : 
+    m_max_depth{0}, 
+    m_depth_limit{depth_limit}, 
+    m_max_triangles_per_node{max_triangles_per_node},
+    m_max_triangles_per_leaf{max_triangles_per_leaf}, 
+    m_keep_triangles_after_this_many_overlaps{m_keep_triangles_after_this_many_overlaps}
+{
     std::vector<glm::vec3> combined_vertecies{};
     std::vector<glm::uvec3> combined_triangle_indecies{};
 
@@ -203,11 +266,18 @@ Octree::Octree(std::vector<Mesh> meshes, size_t depth_limit, size_t max_triangle
 
     m_root = std::make_unique<OctreeNode>(OctreeNode{AABB{min_bounds, max_bounds}, combined_triangle_indecies, {}, true});
     
-    Subdivide(m_root, 0);
+    Subdivide(m_root, 1);
 
     std::cout << "............................................................." << std::endl;
 
-    Traverse(m_root, 0);
+    DepthFirstTraverse(m_root, 0);
+
+    DepthFirstCompress(m_root, 0);
+    
+    std::cout << "compressed node size: " << m_compressed_node_buffer.size() << std::endl;
+    std::cout << "compresed triangle size: " << m_compressed_triangle_indecies.size() << std::endl;
+    std::cout << "uncompressed triangle size: " << combined_triangle_indecies.size() << std::endl;
+    std::cout << "max depth: " << m_max_depth << std::endl;
 }
 
 Octree::~Octree() {}
